6d2e63a66f51491cc6918e74cc82aee66ed09c30
Curtis Badke <curtis.badke@devfacto.com>
2016-01-29 13:48:55 -0700

    Minimal example of using Mde

---
+++ Mde-test/Mde-test/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Mde_test
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/Scripts/simplemde.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/simplemde.min.css"));
+        }
+    }
+}
